2cf07227b88d4087674e08a05e0ced8669f3bb05
AshanOdi <ashanuni25@gmail.com>
2025-09-05 15:17:26 +0530

    api call simple fuction test

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 import express from "express";
 
-let app = express();
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "this is response of get request",
+  });
+});
+
+app.delete("/", (req, res) => {
+  res.json({
+    message: "this is response of delete request",
+  });
+});
+
+app.post("/", (req, res) => {
+  res.json({
+    message: "this is response of post request",
+  });
+});
+
+app.put("/", (req, res) => {
+  res.json({
+    message: "this is response of put request",
+  });
+});
+
+app.listen(5000, () => {
+  console.log("This server is running on Port 5000");
+});
