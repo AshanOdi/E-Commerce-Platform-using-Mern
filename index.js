ff2c6bbbdcfda61d4a86977b99b773784c2a4a9f
AshanOdi <ashanuni25@gmail.com>
2025-09-05 18:14:06 +0530

    database Connected

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.json({
-    message: "this is response of get request",
+app.use(bodyParser.json());
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:1234@cluster0.8uomnzt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    console.log("connected to database");
+  })
+  .catch(() => {
+    console.log("database connection error");
   });
+
+app.get("/", (req, res) => {
+  console.log(req.body.name);
 });
 
 app.delete("/", (req, res) => {
-  res.json({
-    message: "this is response of delete request",
-  });
+  console.log(req.body);
 });
 
 app.post("/", (req, res) => {
@@ -29,3 +40,5 @@ app.put("/", (req, res) => {
 app.listen(5000, () => {
   console.log("This server is running on Port 5000");
 });
+
+// mongodb+srv://admin:1234@cluster0.8uomnzt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
