f13f4ba1d08b7690bc942eade560d7a8129cd52e
AshanOdi <ashanuni25@gmail.com>
2025-09-07 00:02:42 +0530

    refactor product and user controllers to implement admin checks and improve product retrieval logic

--- controller/productController.js
+++ controller/productController.js
@@ -1,4 +1,5 @@
 import Product from "../models/product.js";
+import { isAdmin } from "./userController.js";
 
 export async function getProduct(req, res) {
   //   Product.find().then((data) => {
@@ -11,41 +12,34 @@ export async function getProduct(req, res) {
   //         })
   //     }
   //   );
-
   try {
-    const product = await Product.find();
-    req.json(product);
+    if (isAdmin(req)) {
+      const products = await Product.find();
+      res.json(products);
+    } else {
+      const products = await Product.find({ isAvailable: true });
+      res.json(products);
+    }
   } catch (err) {
     res.json({
       message: "Failed to get Products",
-      error: err,
+      error: err.message,
     });
   }
 }
 
 export function saveProduct(req, res) {
   //   console.log(req.user);
-  if (req.user == null) {
-    res.status(403).json({
-      message: "Unauthorized",
-    });
-    return;
-  }
-
-  if (req.user.role != "admin") {
-    res.status(403).json({
-      message: "Unauthorized You need to be an admin",
-    });
-    return;
-  }
   //   console.log(req.body);
 
-  const product = new Product({
-    name: req.body.name,
-    price: req.body.price,
-    description: req.body.description,
-    image: req.body.image,
-  });
+  if (!isAdmin(req)) {
+    res.status(403).json({
+      message: "You are not authorized to add product",
+    });
+    return;
+  }
+
+  const product = new Product(req.body);
 
   product
     .save()
@@ -57,28 +51,6 @@ export function saveProduct(req, res) {
     .catch(() => {
       res.json({
         message: "Product adding fail",
-      });
-    });
-}
-
-export function saveStudent(req, res) {
-  const student = new Student({
-    name: req.body.name,
-    age: req.body.age,
-    stream: req.body.stream,
-    email: req.body.email,
-  });
-
-  student
-    .save()
-    .then(() => {
-      res.json({
-        message: "Successfully Added Student",
-      });
-    })
-    .catch(() => {
-      res.json({
-        message: "Failed to Add Student",
       });
     });
 }
